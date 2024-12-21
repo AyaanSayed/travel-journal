e62dbbe3b6993b12f6a23d34aff35256246a0cfb
AyaanSayed <138655396+AyaanSayed@users.noreply.github.com>
2024-12-21 23:44:24 +0530

    added reusable component card

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
-export default function Header(){
-  return <header style={{backgroundColor: "#F55A5A", display:"flex", justifyContent:"center", alignItems: "center", height: 55 }}>
+export default function Header() {
+  return <header style={{ backgroundColor: "#F55A5A", display: "flex", justifyContent: "center", alignItems: "center", height: 55 }}>
     <img width={24} src="src/assets/globe.png" alt="globe-icon" />
-    <h1 style={{marginLeft: 7, fontSize:16, color: "white", fontWeight: 500}}>my travel journal.</h1>
+    <h1 style={{ marginLeft: 7, fontSize: 16, color: "white", fontWeight: 500 }}>my travel journal.</h1>
   </header>
 }
